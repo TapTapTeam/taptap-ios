db055e4f3496a24337a5da142414129d474927d1
Hong <dbsghdz1@gmail.com>
2026-01-02 17:09:23 +0900

    [#1] fix: Nbs Test수정

--- TapTap/Tuist/ProjectDescriptionHelpers/TargetName.swift
+++ TapTap/Tuist/ProjectDescriptionHelpers/TargetName.swift
@@ -8,15 +8,11 @@
 import ProjectDescription
 
 public enum TargetName: String {
-  case Nbs
-  case NbsTests
   case SafariExtension
   case ShareExtension
   
   public var sourcesPath: String {
     switch self {
-    case .Nbs: return "Nbs/Sources/**"
-    case .NbsTests: return "Nbs/Tests/**"
     case .SafariExtension: return "SafariExtension/Sources/**"
     case .ShareExtension: return "ShareExtension/Sources/**"
     }
@@ -24,8 +20,6 @@ public enum TargetName: String {
   
   public var resourcesPath: String {
     switch self {
-    case .Nbs: return "Nbs/Resources/**"
-    case .NbsTests: return ""
     case .SafariExtension: return "SafariExtension/Resources/**"
     case .ShareExtension: return "ShareExtension/Resources/**"
     }
