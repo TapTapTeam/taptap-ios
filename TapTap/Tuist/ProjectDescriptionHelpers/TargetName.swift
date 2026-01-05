2a5cf2706df6d9dc8adbd5d5137003ad7117458e
Hong <dbsghdz1@gmail.com>
2026-01-05 18:11:53 +0900

    [#1] fix: 번들아이디에 맞게 소문자로 변경

--- TapTap/Tuist/ProjectDescriptionHelpers/TargetName.swift
+++ TapTap/Tuist/ProjectDescriptionHelpers/TargetName.swift
@@ -8,20 +8,20 @@
 import ProjectDescription
 
 public enum TargetName: String {
-  case SafariExtension
-  case ShareExtension
+  case safariExtension
+  case shareExtension
   
   public var sourcesPath: String {
     switch self {
-    case .SafariExtension: return "SafariExtension/Sources/**"
-    case .ShareExtension: return "ShareExtension/Sources/**"
+    case .safariExtension: return "SafariExtension/Sources/**"
+    case .shareExtension: return "ShareExtension/Sources/**"
     }
   }
   
   public var resourcesPath: String {
     switch self {
-    case .SafariExtension: return "SafariExtension/Resources/**"
-    case .ShareExtension: return "ShareExtension/Resources/**"
+    case .safariExtension: return "SafariExtension/Resources/**"
+    case .shareExtension: return "ShareExtension/Resources/**"
     }
   }
 }
