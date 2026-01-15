e80e8e883780bb8e66daa73e8cc749a66d739974
YeoSeongil <seongil_yeo@naver.com>
2026-01-15 19:58:34 +0900

    [#9] chore: OnboardingHighlightTip 수정
    불필요한 변수 삭제 및 칩버튼 클릭 클로저 구현

--- TapTap/Projects/DesignSystem/Sources/Components/Onboarding/OnboardingHighlightTip.swift
+++ TapTap/Projects/DesignSystem/Sources/Components/Onboarding/OnboardingHighlightTip.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 public struct OnboardingHighlightTip {
-  @Binding var selectedColor: Color
+  var onChipTapped: () -> Void
   var onMemoTapped: () -> Void
   
-  public init(selectedColor: Binding<Color>, onMemoTapped: @escaping () -> Void) {
-    self._selectedColor = selectedColor
+  public init(
+    onChipTapped: @escaping () -> Void,
+    onMemoTapped: @escaping () -> Void
+  ) {
+    self.onChipTapped = onChipTapped
     self.onMemoTapped = onMemoTapped
   }
 }
@@ -21,39 +24,38 @@ extension OnboardingHighlightTip: View {
   public var body: some View {
     VStack(spacing: 0) {
       HStack(spacing: 6) {
-        Button(action: { selectedColor = .chipPink }) {
+        Button(action: onChipTapped) {
           Capsule()
             .fill(.chipPink)
             .overlay(
               Capsule().strokeBorder(
-                selectedColor == .chipPink ? .chipPinkLine : .stateDefaultLine,
+                .chipPinkLine,
                 lineWidth: 2
               )
             )
             .frame(width: 50, height: 40)
         }
-        Button(action: { selectedColor = .chipYellow }) {
-          Capsule()
-            .fill(.chipYellow)
-            .overlay(
-              Capsule().strokeBorder(
-                selectedColor == .chipYellow ? .chipYellowLine : .stateDefaultLine,
-                lineWidth: 2
-              )
+        
+        Capsule()
+          .fill(.chipYellow)
+          .overlay(
+            Capsule().strokeBorder(
+              .stateDefaultLine,
+              lineWidth: 2
             )
-            .frame(width: 50, height: 40)
-        }
-        Button(action: { selectedColor = .chipBlue }) {
-          Capsule()
-            .fill(.chipBlue)
-            .overlay(
-              Capsule().strokeBorder(
-                selectedColor == .chipBlue ? .chipBlueLine : .stateDefaultLine,
-                lineWidth: 2
-              )
+          )
+          .frame(width: 50, height: 40)
+        
+        Capsule()
+          .fill(.chipBlue)
+          .overlay(
+            Capsule().strokeBorder(
+              .stateDefaultLine,
+              lineWidth: 2
             )
-            .frame(width: 50, height: 40)
-        }
+          )
+          .frame(width: 50, height: 40)
+        
         Button(action: onMemoTapped) {
           ZStack {
             Capsule()
@@ -76,49 +78,15 @@ extension OnboardingHighlightTip: View {
       .clipShape(RoundedRectangle(cornerRadius: 30))
       .shadow(color: Color.bgShadow4, radius: 3, x: 0, y: 2)
       .shadow(color: Color.bgShadow5, radius: 10, x: 0, y: 0)
-      Triangle()
-        .fill(.stateTooltipbackground)
-        .frame(width: 16, height: 10)
-        .shadow(color: Color.bgShadow4, radius: 8, x: 0, y: 6)
-        .shadow(color: Color.bgShadow5, radius: 4, x: 0, y: 6)
     }
-//    .shadow(color: Color.bgShadow4, radius: 3, x: 0, y: 2)
-//    .shadow(color: Color.bgShadow5, radius: 10, x: 0, y: 0)
   }
 }
 
 #Preview {
   struct PreviewWrapper: View {
-    @State private var color: Color = .chipPink
     var body: some View {
-      OnboardingHighlightTip(selectedColor: $color, onMemoTapped: {})
+      OnboardingHighlightTip(onChipTapped: {}, onMemoTapped: {})
     }
   }
   return PreviewWrapper()
-}
-
-fileprivate struct Triangle: Shape {
-  var cornerRadius: CGFloat = 2
-  
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
-    
-    let top = CGPoint(x: rect.midX, y: rect.maxY)
-    let bottomLeft = CGPoint(x: rect.minX, y: rect.minY)
-    let bottomRight = CGPoint(x: rect.maxX, y: rect.minY)
-    
-    path.move(to: bottomLeft)
-    
-    path.addArc(
-      tangent1End: top,
-      tangent2End: bottomRight,
-      radius: cornerRadius
-    )
-    
-    path.addLine(to: bottomRight)
-    path.addLine(to: bottomLeft)
-    
-    path.closeSubpath()
-    return path
-  }
 }
