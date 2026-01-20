d76aa24024a5b537db8ecd002eadb1ed0c48e197
Hong <dbsghdz1@gmail.com>
2026-01-20 17:27:37 +0900

    [#13] fix: 알럿UI 변경

--- TapTap/Projects/App/SafariExtension/Resources/gesture.js
+++ TapTap/Projects/App/SafariExtension/Resources/gesture.js
@@ -23,13 +23,11 @@ TapTap.gesture = {
       const wrapper = event.target.closest('.taptap-wrapper');
       if (wrapper) {
         const highlightId = wrapper.dataset.highlightId;
-        
-        // [추가] 하이라이트에 메모가 있는지 확인합니다.
+
         const containerId = 'capsules-for-' + highlightId;
         const capsuleContainer = document.getElementById(containerId);
         const hasMemos = capsuleContainer && capsuleContainer.childElementCount > 0;
 
-        // 하이라이트와 연결된 메모 컨테이너를 함께 삭제하는 함수
         const deleteHighlightAndMemos = () => {
           if (capsuleContainer) {
             capsuleContainer.remove();
@@ -38,21 +36,18 @@ TapTap.gesture = {
         };
 
         if (hasMemos) {
-          // [수정] 커스텀 확인창을 사용합니다.
           TapTap.customAlert.show(
-            "메모가 있는 하이라이트입니다. 정말 삭제하시겠습니까?",
-            () => { // 사용자가 '삭제'를 눌렀을 때 실행될 콜백 함수
+            "삭제한 메모는 복구할 수 없어요",
+            () => {
               deleteHighlightAndMemos();
             }
           );
         } else {
-          // 메모가 없으면, 바로 삭제합니다.
           deleteHighlightAndMemos();
         }
       } else {
         TapTap.sentence.selectSentenceAt(event);
       }
-      
       event.preventDefault();
     }
     
