a5db412bf914893a73586aed14313fad1271fabc
YeoSeongil <seongil_yeo@naver.com>
2026-01-25 13:00:45 +0900

    [#24] add: 예제앱 파일 추가

---
+++ TapTap/Projects/Feature/OnboardingFeature/Example/Example.swift
@@ -0,0 +1,17 @@
+//
+//  Example.swift
+//  HomeFeature
+//
+//  Created by 여성일 on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Example: App {
+  var body: some Scene {
+    WindowGroup {
+      
+    }
+  }
+}
