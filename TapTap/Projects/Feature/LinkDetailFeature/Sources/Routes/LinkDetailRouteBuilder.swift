7a6fe76c133b151bb0129d4d3a5a0acadc933cec
YeoSeongil <seongil_yeo@naver.com>
2026-02-03 01:41:36 +0900

    [#33] tuist: LinkDetail 모듈 마이그레이션

---
+++ TapTap/Projects/Feature/LinkDetailFeature/Sources/Routes/LinkDetailRouteBuilder.swift
@@ -0,0 +1,33 @@
+//
+//  LinkDetailRouteBuilder.swift
+//  Feature
+//
+//  Created by 이안 on 10/28/25.
+//
+
+import ComposableArchitecture
+import LinkNavigator
+
+import Domain
+import Shared
+
+public struct LinkDetailRouteBuilder {
+  
+  public init() {}
+  
+  @MainActor
+  public func generate() -> RouteBuilderOf<SingleLinkNavigator> {
+    let matchPath = Route.linkDetail.rawValue
+    return .init(matchPath: matchPath) { navigator, item, _ -> RouteViewController? in
+      
+      let query: ArticleItem? = item.decoded()
+  
+      return WrappingController(matchPath: matchPath) {
+        LinkDetailView(store: Store(initialState: LinkDetailFeature.State(article: query!)) {
+          LinkDetailFeature()
+            .dependency(\.linkNavigator, .init(navigator: navigator))
+        })
+      }
+    }
+  }
+}
