14341dc920ec6b14f04ee9be570fe557f2f2b6ea
YeoSeongil <seongil_yeo@naver.com>
2026-02-03 01:39:12 +0900

    [#33] tuist: setting 모듈 마이그레이션

---
+++ TapTap/Projects/Feature/SettingFeature/Sources/Reducers/PolicyDetailFeature.swift
@@ -0,0 +1,47 @@
+//
+//  PolicyDetailFeature.swift
+//  Feature
+//
+//  Created by 이안 on 11/5/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+import LinkNavigator
+
+import Domain
+import Shared
+
+@Reducer
+public struct PolicyDetailFeature {
+  @Dependency(\.linkNavigator) var linkNavigator
+  
+  @ObservableState
+  public struct State: Equatable {
+    public var title: String
+    public var text: String
+    
+    public init(title: String, text: String) {
+      self.title = title
+      self.text = text
+    }
+  }
+  
+  public enum Action: Equatable {
+    case backButtonTapped
+  }
+  
+  public init() {}
+  
+  public var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .backButtonTapped:
+        return .run { _ in
+          await linkNavigator.pop()
+        }
+      }
+    }
+  }
+}
