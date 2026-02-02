7110c039e411276bbc465d25505c7c0629ef9af8
YeoSeongil <seongil_yeo@naver.com>
2026-02-02 13:58:53 +0900

    [#33] add: 누락 파일 추가

--- TapTap/Projects/Feature/MyCategoryFeature/Project.swift
+++ TapTap/Projects/Feature/MyCategoryFeature/Project.swift
@@ -15,12 +15,12 @@ let project = Project.project(
       name: FeatureModule.MyCategoryFeature.rawValue,
       product: .staticFramework,
       sources: .sources,
+      resources: .default,
       dependencies: [
         .TCA(),
         .domain(),
         .LinkNavigator(),
         .Lottie(),
-        .myCategoryClient()
       ]
     ),
     Target.target(
@@ -28,7 +28,6 @@ let project = Project.project(
       product: .app,
       infoPlist: .default,
       sources: ["Example/**"],
-      resources: .default,
       dependencies: [
         .target(name: FeatureModule.MyCategoryFeature.rawValue)
       ]
