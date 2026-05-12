a0ddf2685b8f85aadb082fae5be4fcb08778cd09
Seungjin Lee <sjin3687@gmail.com>
2026-05-12 15:46:51 +0900

    [#95] feat: MacPopup 구현
    DesignSystem

---
+++ TapTap/Projects/DesignSystem/Sources/macOS/Popup/MacPopupVariant.swift
@@ -0,0 +1,18 @@
+//
+//  MacPopupVariant.swift
+//  DesignSystem
+//
+//  Created by 이승진 on 5/12/26.
+//
+
+import Foundation
+
+#if os(macOS)
+
+/// `MacPopupItem`의 시각적 스타일을 정의하는 enum입니다.
+public enum MacPopupVariant {
+  case normal
+  case danger
+}
+
+#endif
