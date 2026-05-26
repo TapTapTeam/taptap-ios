a66a312a0ae3397e6b468003349b7fbded3118cc
Seungjin Lee <sjin3687@gmail.com>
2026-05-26 21:47:34 +0900

    [#95] chore: LinkDetailFeature 추가

---
+++ TapTap/Projects/MacFeature/LinkDetailFeature/Tests/LinkDetailTests.swift
@@ -0,0 +1,15 @@
+//
+//  LinkDetailTests.swift
+//  MacLinkDetailFeatureTests
+//
+//  Created by TapTap on 5/11/26.
+//
+
+import XCTest
+@testable import MacLinkDetailFeature
+
+final class LinkDetailTests: XCTestCase {
+  func test_example() {
+    XCTAssertTrue(true)
+  }
+}
