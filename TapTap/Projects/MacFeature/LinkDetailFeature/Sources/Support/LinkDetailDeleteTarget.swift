c2fd3adac7ca2f942debb3e4b2271fe1a0307342
Seungjin Lee <sjin3687@gmail.com>
2026-06-03 16:01:59 +0900

    [#95] feat: LinkDetailDeleteTarget 구현

---
+++ TapTap/Projects/MacFeature/LinkDetailFeature/Sources/Support/LinkDetailDeleteTarget.swift
@@ -0,0 +1,35 @@
+//
+//  LinkDetailDeleteTarget.swift
+//  MacLinkDetailFeature
+//
+//  Created by 이승진 on 5/27/26.
+//
+
+/// 삭제 확인 다이얼로그가 처리할 대상과 문구를 정의합니다.
+enum LinkDetailDeleteTarget: Equatable {
+  case article
+  case highlight(String)
+  case comment(highlightID: String, commentID: Double)
+
+  var alertTitle: String {
+    switch self {
+    case .article:
+      return "이 링크를 삭제할까요?"
+    case .highlight:
+      return "이 하이라이트를 삭제할까요?"
+    case .comment:
+      return "해당 메모를 삭제할까요?"
+    }
+  }
+
+  var alertMessage: String {
+    switch self {
+    case .article:
+      return "삭제한 링크는 복구할 수 없어요"
+    case .highlight:
+      return "삭제한 하이라이트는 복구할 수 없어요"
+    case .comment:
+      return "삭제된 메모는 복구할 수 없어요"
+    }
+  }
+}
