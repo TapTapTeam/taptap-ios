d38682393c3136197eeece624ed83c4cf60ef5fd
YeoSeongil <seongil_yeo@naver.com>
2026-07-15 14:17:17 +0900

    [#110] design: 셋팅 페이지에 사용할 에셋 추가

--- TapTap/Projects/DesignSystem/Sources/NameSpace/Icon.swift
+++ TapTap/Projects/DesignSystem/Sources/NameSpace/Icon.swift
@@ -82,6 +82,12 @@ public extension MacIcon {
   static let squareEdit = "mac_square_edit"
   static let trashBold = "mac_trash_bold"
   static let link = "mac_link"
+  static let file = "mac_file"
+  static let heart = "mac_heart"
+  static let info = "mac_info"
+  static let opensource = "mac_opensource"
+  static let shield = "mac_shield"
+  static let chevron_right = "mac_chevron_right"
 }
 
 public extension Image {
