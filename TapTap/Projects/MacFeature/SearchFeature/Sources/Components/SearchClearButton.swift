c062670018063910fbfe4f7416ddd0e056984c6a
YeoSeongil <seongil_yeo@naver.com>
2026-08-30 08:49:18 +0900

    design: 검색어 삭제 버튼 에셋 추가

---
+++ TapTap/Projects/MacFeature/SearchFeature/Sources/Components/SearchClearButton.swift
@@ -0,0 +1,33 @@
+//
+//  SearchClearButton.swift
+//  MacSearchFeature
+//
+//  Created by 여성일 on 8/30/26.
+//
+
+import SwiftUI
+import DesignSystem
+
+public struct SearchClearButton: View {
+  private let action: () -> Void
+
+  public init(action: @escaping () -> Void) {
+    self.action = action
+  }
+}
+
+public extension SearchClearButton {
+  var body: some View {
+    Button(action: action) {
+      Image(icon: MacIcon.query_delete)
+        .resizable()
+        .renderingMode(.template)
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 17.97, height: 17.97)
+        .foregroundStyle(.iconDisabled)
+        .frame(width: 20, height: 20)
+        .contentShape(Rectangle())
+    }
+    .buttonStyle(.plain)
+  }
+}
